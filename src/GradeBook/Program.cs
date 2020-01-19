11b673f277907d879ba76e2384dc991620656ee9
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2020-01-19 15:25:55 -0300

    Executado comandos CLI
    -VIsto como debugar um código.
    -Como receber parametros.
    - Com tratar excessões simples. (if else)

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,9 +7,14 @@ namespace GradeBook
         //Metodo principal do programa. parametro args é um array de strings
         static void Main(string[] args)
         {
-            //Utilizando interpolação de Strings com expressão C# $"{}"
-            Console.WriteLine($"Hello {args[0]} !");
 
+            if(args.Length > 0) {
+                //Utilizando interpolação de Strings com expressão C# $"{}"
+                Console.WriteLine($"Hello {args[0]} !");
+            }else {
+                Console.WriteLine("Hello Stranger!");
+            }
+            
             //Utilizando concatenação de Strings
             //Console.WriteLine("Hello" + args[0] + "!");
         }
